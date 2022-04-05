88ba5ad8553604c512228911db1647dbda9315bf
opang456 <naufalwildan459@gmail.com>
2022-04-05 21:06:42 +0800

    POSTTEST 2 037

---
+++ 2009106037_Muhammad Naufal Wildan/src/POSTTEST2/keuangan.java
@@ -0,0 +1,26 @@
+package POSTTEST2;
+
+public class keuangan {
+    String nama_instansi;
+    int anggaran_masuk;
+    int anggaran_keluar;
+    int anggaran_perbulan;
+    int anggaran_pertahun;
+    
+    public keuangan(String nama_instansi,int anggaran_masuk,int anggaran_keluar,int anggaran_perbulan,int anggaran_pertahun){
+        this.nama_instansi = nama_instansi;
+        this.anggaran_masuk = anggaran_masuk;
+        this.anggaran_keluar = anggaran_keluar;
+        this.anggaran_perbulan = anggaran_perbulan;
+        this.anggaran_pertahun = anggaran_pertahun;
+    }
+    
+    void diterima(){
+        System.out.println("Anggaran Diterima");
+    }
+    
+    void ditolak(){
+        sout
+    }
+    
+}
